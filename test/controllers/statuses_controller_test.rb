0cdeb19305646912bc4d1ed06125e6e32e420520
brunoban <bruno.bandeira@gmail.com>
2016-02-08 03:27:57 -0200

    Added status validation and itnegration tests.

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -23,8 +23,15 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should be logged in to post the statuses" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 
-  test "should create status" do
+
+  test "should create status when logged in" do
+    sign_in users(:bruno)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -37,12 +44,21 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:bruno)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not logegd in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+
+  test "should update status when logged in" do
+    sign_in users(:bruno)
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
